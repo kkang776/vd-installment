9ac0b5108a5946b92a4e4e974f77e7ab550babe9
kkang776 <kkang776@gmail.com>
2026-04-30 20:24:29 +0900

    fix: resolve JSX tag mismatch in PaymentForm and add missing prisma lib

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -506,6 +506,7 @@ export default function PaymentForm({ onOpenModal, onOpenProductDetail }: {
               </button>
             </div>
           </div>
+        </div>
         </aside>
     </form>
 
