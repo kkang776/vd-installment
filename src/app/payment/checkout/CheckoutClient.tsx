f3ddd5ac432eb76dc21b131e31505f88da4984ad
kkang776 <kkang776@gmail.com>
2026-06-26 15:21:46 +0900

    card2

--- src/app/payment/checkout/CheckoutClient.tsx
+++ src/app/payment/checkout/CheckoutClient.tsx
@@ -597,6 +597,8 @@ export default function CheckoutClient({ initialOrder }: { initialOrder: Order }
         <input type="hidden" name="ordr_chk" value="" />
 
         {/* Card restrictions & installment */}
+        <input type="hidden" name="kcp_noint" id="kcp_noint" value="Y" />
+        <input type="hidden" name="kcp_noint_quota" id="kcp_noint_quota" value="CCBC-02:03:04:05:06:07:08:09:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:25:26:27:28:29:30:31:32:33:34:35:36,CCLO-02:03:04:05:06:07:08:09:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:25:26:27:28:29:30:31:32:33:34:35:36,CCWR-02:03:04:05:06:07:08:09:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:25:26:27:28:29:30:31:32:33:34:35:36,CCKB-02:03:04:05:06:07:08:09:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:25:26:27:28:29:30:31:32:33:34:35:36,CCHN-02:03:04:05:06:07:08:09:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:25:26:27:28:29:30:31:32:33:34:35:36" />
         <input type="hidden" name="used_card" id="used_card" value="" />
         <input type="hidden" name="used_card_YN" value="Y" />
       </form>
